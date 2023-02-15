19d0e57513daf278647de5d8a726bfdca6fbfca2
Kenfresno <Kfresno@proton.me>
2023-02-15 21:35:39 +0200

    operator and equals buttons with JS

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
+//Basic operations for calculator//
 const add = function(x,y) {
-        return x + y;
+    return x + y;
 };
-
 const substract = function(x,y) {
     return x-y;
 };
-
-const sum = function(x,y) {
-    if (x.length == 0) return 0;
-    if (x.length === 1) return x[0];
-    else if (x.length > 1) return x.pop()+sum(x);
-    return x + y;
-};
-
 const multiply = function(x,y) {
     return x * y;
 };
-
 const divide = function(x,y) {
     return x / y;
 };
 
+//Initialize two variables to hold numbers//
+var x = 0;
+var y = 0;
+//Initialize variable to hold which operator user selected, set initial result to 0//
+var operator = "";
+var result = 0;
+
+//First check if user clicked the operator-buttons//
+//If any condition is true, store current value on calculator screen//
+//Store selected operator and clear the input screen//
 function opr(val){
-    document.getElementById("result").value += val
+    if (val === "+"){
+        operator = val;
+        x = +document.getElementById("result").value;
+        document.getElementById("result").value = "";  
+        return;      
+    } else if (val === "-"){
+        operator = val;
+        x = +document.getElementById("result").value;
+        document.getElementById("result").value = ""; 
+        return;       
+    } else if (val === "*"){
+        operator = val;
+        x = +document.getElementById("result").value;
+        document.getElementById("result").value = "";
+        return;        
+    } else if (val === "/"){
+        operator = val;
+        x = +document.getElementById("result").value;
+        document.getElementById("result").value = "";
+        return;   
+    }
+    document.getElementById("result").value += val;
+    y = +document.getElementById("result").value;
 };
 
 function clr(){
     document.getElementById("result").value = ""
+    x = 0;
+    y = 0;
+    operator = "";
+};
+
+function solve(){
+    if (operator === "+"){
+        result = add(x,y);
+        document.getElementById("result").value = result;
+    } else if (operator === "-") {
+        result = substract(x,y);
+        document.getElementById("result").value = result;
+    } else if (operator === "*") {
+        result = multiply(x,y);
+        document.getElementById("result").value = result;
+    } else if (operator === "/") {
+        result = divide(x,y);
+        document.getElementById("result").value = result;
+    } else return
 };
